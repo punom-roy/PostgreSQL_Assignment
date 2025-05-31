c00f25cd05bc5e085fdf012a4afff25368c65c43
Punom Roy <128087525+punom-roy@users.noreply.github.com>
2025-05-31 15:06:44 +0600

    little changes

--- PostgreSQL_Assignment-2.sql
+++ PostgreSQL_Assignment-2.sql
@@ -42,6 +42,7 @@ INSERT INTO species (species_id, common_name, scientific_name, discovery_date, c
 (3, 'Red Panda', 'Ailurus fulgens', '1825-01-01', 'Vulnerable'),
 (4, 'Asiatic Elephant', 'Elephas maximus indicus', '1758-01-01', 'Endangered');
 
+-- Insert sightings DATA
 INSERT INTO sightings (sighting_id, species_id, ranger_id, location, sighting_time, notes) VALUES
 (1, 1, 1, 'Peak Ridge', '2024-05-10 07:45:00', 'Camera trap image captured'),
 (2, 2, 2, 'Bankwood Area', '2024-05-12 16:20:00', 'Juvenile seen'),
